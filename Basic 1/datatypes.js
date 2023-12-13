06392f9e88b381dd95c80c6917a091758c983cbf
Neo01 <rkrai3122@gmail.com>
2023-12-13 08:16:36 +0530

    Updation

---
+++ Basic 1/datatypes.js
@@ -0,0 +1,16 @@
+//arry
+
+const myArr = ["A" , "B" , "C" , "D" , "E" , "F" , "G" , "H"]
+const myHero = ["RAKA" , "Kilvish"]
+
+const myArr2 = new Array(1,2,3,4)
+
+
+// slice , spice 
+// console.log("A" ,myArr);
+const myN1 = myArr.slice(0 ,3)
+// console.log(myArr)
+// console.log("B", myN1);
+
+const myN2 = myArr.splice(1,3)
+console.log(myN2);
