5c00012436a7af759df4501d09e81de93d121490
Neo01 <rkrai3122@gmail.com>
2024-01-10 11:45:26 +0530

    practice

---
+++ ControlFlow/Switch.js
@@ -0,0 +1,18 @@
+const Day = "Monday"
+
+
+switch (Day) {
+    case "Tuesday":
+        console.log("True");
+        break;
+    case "Monday":
+        console.log("False");
+        break;
+    case "Wednesday":
+        console.log("!True");
+        break;
+
+
+    default:
+        break;
+}
---
+++ ControlFlow/truthy.js
@@ -0,0 +1,53 @@
+// const userEmail = [1,2]
+
+// if (userEmail){
+//     console.log("got email");
+// }
+
+// else {
+//     console.log("Dont have Email");
+// }
+
+// -------------------------------Rules----------------------
+
+
+//Falsy vales
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NAN
+
+//truthy Values
+
+//"0" , "False", " ",[], {}, function(){}
+
+
+// if (userEmail.length === 0){
+//     console.log("Array is true");
+// }
+
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length == 0){
+//     console.log("Empty");
+// }
+
+
+// -----------------Nullish Coalescing Operator(??): null undefined 
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 5
+// // val1 = undefined ?? 15
+// val1 = null ?? undefined ?? 10
+
+
+
+// console.log(val1)
+
+// ------------------Terniary operator------------------------------------
+
+// condition ? true : true
+
+// const iceTeaPrice = 100
+
+// iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("more than 80");v 
